c237cd6e2c50e6853afd9f6f5ed67cddaebd77de
tenXisss <bogdansport14@gmail.com>
2026-02-24 13:43:30 +0300

    feat: реализован класс SumDoubleHex для шестнадцатеричных чисел

---
+++ src/ru/hse/java2026/SumDoubleHex.java
@@ -0,0 +1,44 @@
+package ru.hse.java2026;
+
+public class SumDoubleHex {
+    public static void main(String[] args) {
+        if (args == null || args.length == 0) {
+            System.out.println(0.0);
+            return;
+        }
+
+        double totalSum = 0.0; 
+
+        for (String arg : args) {
+            if (arg == null) {
+                continue;
+            }
+
+            int i = 0;
+            while (i < arg.length()) {
+                while (i < arg.length() && Character.isWhitespace(arg.charAt(i))) { //isWhitespace для обработки юникода
+                    i++;
+                }
+
+                if (i >= arg.length()) {
+                    break;
+                }
+
+                int start = i;
+                
+                while (i < arg.length() && !Character.isWhitespace(arg.charAt(i))) {
+                    i++;
+                }
+
+                String part = arg.substring(start, i);
+                try {
+                    totalSum += Double.parseDouble(part); // parseDouble для работы с префиксом
+                } catch (NumberFormatException e) {
+                    System.err.println("Пропущено некорректное значение: " + part);
+                }
+            }
+        }
+
+        System.out.println(totalSum);
+    }
+}
